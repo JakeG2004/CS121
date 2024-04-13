59e7c5b9001b61cf05f9cdc7f95778fa3c041aad
Jake Gendreau <jake.lgendreau@gmail.com>
2024-04-13 13:35:37 -0700

    imp 1

--- Assignments/Trees/log.md
+++ Assignments/Trees/log.md
@@ -6,3 +6,8 @@
 10:48 PM - Finished first version of BSTree.cpp
 11:16 PM - Wrote test code. It seems to work. I'm gonna call it a night here. About 2 hours in so far and I got all of the tree logic working YIPEEEE!! I also need to update the functions so they don't have to refer to their own head in the function call. It's kind of nasty rn.
 11:30 PM - Updated deleteTree() to actually free all of the memory that was being used.
+
+## April 13, 2024
+11:30 AM - Started working on the actual project code
+1:06 PM - I got most of it working, but the actors don't seem to want to work very well. It's really odd. I also should decouple a lot of my functions from the data structure
+1:13 PM - It doesn't work well because there is whitespace around a lot of the names. I need to make something that removes whitespace from the end of a string
---
+++ Assignments/Trees/showDB.cpp
@@ -0,0 +1,205 @@
+/*
+showDB.cpp
+A file to read in shows from large.dat and store them in a tree
+Jake Gendreau
+April 13, 2024
+*/
+
+#include <iostream>
+#include <fstream>
+#include "BSTree.cpp"
+
+using namespace std;
+
+BSTree readFile(string);
+
+string getName(string);
+
+int getStartDate(string);
+int getEndDate(string);
+
+void insertActor(NodePtr&, string);
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        cout << "Incorrect usage! Usage: ./a.out <path_to_datafile>. Exiting program..." << endl;
+        exit(-1);
+    }
+
+    //read in the tree from the data file
+    BSTree showTree = readFile(argv[1]);
+
+    //cout << "All show titles in the tree:" << endl;
+    //showTree.printShows(showTree.head);
+
+    cout << "All actors in Parry Mason" << endl;
+    showTree.printActorsInShow(showTree.head, "Perry Mason");
+
+    //the issue here is that i only account for one space when adding the name, not multiple
+    cout << "All actors in The Office" << endl;
+    showTree.printActorsInShow(showTree.head, "The Office ");
+
+    cout << "All actors in The Prisoner" << endl;
+    showTree.printActorsInShow(showTree.head, "The Prisoner");
+
+    cout << "All actors in Gilligan's Island" << endl;
+    showTree.printActorsInShow(showTree.head, "Gilligan's Island");
+
+    cout << "All actors in Parry M*A*S*H" << endl;
+    showTree.printActorsInShow(showTree.head, "M*A*S*H");
+
+    cout << "All Shows with Raymond Burr" << endl;
+    showTree.printShowsWithActor(showTree.head, "Raymond Burr");
+
+    //TODO: Handle duplicate cases
+    cout << "All Shows with Bill Mumy" << endl;
+    showTree.printShowsWithActor(showTree.head, "Bill Mumy");
+
+    //TODO: The name has whitespace at the end
+    cout << "All Shows with Bob Newhart" << endl;
+    showTree.printShowsWithActor(showTree.head, "Bob Newhart");
+
+    cout << "All Shows with Jerry Seinfeld" << endl;
+    showTree.printShowsWithActor(showTree.head, "Jerry Seinfeld");
+
+    //TODO: Also doesn't appear when he should
+    cout << "All Shows with Bob Denver" << endl;
+    showTree.printShowsWithActor(showTree.head, "Bob Denver");
+
+}
+
+BSTree readFile(string filepath)
+{
+    BSTree showTree = BSTree();
+    string curLine;
+
+    //open file
+    ifstream infile;
+    infile.open(filepath);
+
+    //ensure that file exists and is open
+    if(!infile.is_open())
+    {
+        cout << "Error opening file at " << filepath << ". Exiting program..." << endl;
+        exit(-1);
+    }
+
+    //While not at the end of the file
+    while(getline(infile, curLine))
+    {
+        //make new show for each 
+        Show newShow = Show();
+
+        // Skip empty lines
+        while(curLine == "") 
+        {
+            if(!getline(infile, curLine)) {
+                // Exit loop if end of file is reached
+                break;
+            }
+        }
+
+        //check for end of file
+        if(infile.eof()) 
+        {
+            break;
+        }
+
+        //get name and dates
+        newShow.name = getName(curLine);
+        newShow.startDate = getStartDate(curLine);
+        newShow.endDate = getEndDate(curLine);
+
+        //get genre
+        getline(infile, curLine);
+        newShow.genre = curLine;
+
+        //get link
+        getline(infile, curLine);
+        newShow.link = curLine;
+
+        //get actors / actresses
+        while(curLine != "")
+        {
+            getline(infile, curLine);
+            insertActor(newShow.actorHead, curLine);
+        }
+
+        showTree.insertNode(showTree.head, newShow);
+    }
+
+    return showTree;
+}
+
+string getName(string curLine)
+{
+    string name = "";
+
+    //read through string, stopping at parenthesis
+    //use curLine[i + 1] to account for the space between the title and dates
+    for(int i = 0; curLine[i + 1] != '('; i++)
+    {
+        name += curLine[i];
+    }
+
+    return name;
+}
+
+int getStartDate(string curLine)
+{
+    string startDate = "";
+
+    //navigate until first date is met, defined by open parens
+    int i = 0;
+    while(curLine[i - 1] != '(')
+    {
+        i++;
+    }
+
+    //add the following four digits to the return string
+    for(int x = 0; x < 4; x++)
+    {
+        startDate += curLine[i + x];
+    }
+
+    //return the stoi conversion of startDate
+    return stoi(startDate);
+}
+
+int getEndDate(string curLine)
+{
+    string endDate = "";
+
+    //get size of string
+    int numChars = 0;
+    for(int i = 0; curLine[i] != ')'; i++)
+    {
+        numChars++;
+    }
+
+    //add the first four of the last 5 characters to the string
+    for(int i = numChars - 4; i < numChars; i++)
+    {
+        endDate += curLine[i];
+    }
+
+    //return the stoi conversion of endDate
+    return stoi(endDate);
+}
+
+void insertActor(NodePtr &head, string actorName)
+{
+    //handle empty name
+    if(actorName == "")
+        return;
+
+    //insert new actor at the beginning
+    NodePtr n = new node();
+
+    n -> data = actorName;
+    n -> next = head;
+
+    head = n;
+}
